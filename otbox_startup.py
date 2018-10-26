e6a3dc0c016191691184d92415065883fe038fe2
bozidars27 <bozho93@gmail.com>
2018-10-26 14:59:52 +0200

    Script `otbox.py` starting with neccessary commands

--- otbox_startup.py
+++ otbox_startup.py
@@ -98,7 +98,7 @@ class OTBoxStartup:
 			for ind, node in enumerate(self.booted_nodes):
 				node_name = 'node-' + node.split('.')[0]
 				print("Starting otbox.py on " + node_name + "...")
-                                self.ssh_command_exec('ssh -o "StrictHostKeyChecking no" root@' + node_name + ' "source /etc/profile; cd A8; cd opentestbed; pip install requests; python otbox.py iotlab >& otbox-' + node_name + '.log &"')
+                                self.ssh_command_exec('ssh -o "StrictHostKeyChecking no" root@' + node_name + ' "source /etc/profile; cd A8; cd opentestbed; pip install requests; python otbox.py --testbed=iotlab --broker=broker.mqttdashboard.com >& otbox-' + node_name + '.log &"')
 
 				self.active_nodes.append(node)
 				self.socketIoHandler.publish('NODE_ACTIVE', node_name)
