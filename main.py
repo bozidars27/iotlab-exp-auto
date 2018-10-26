399188331f19403e6a3f3a7e4a30a2fd2daa4224
bozidars27 <bozho93@gmail.com>
2018-10-26 13:20:14 +0200

    Replacing `sys` with `argparse` for argument parsing, and adding testbed as a new argument

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import sys
+import argparse
 import ConfigParser
 import os
 
@@ -20,28 +20,59 @@ EXP_DURATION = 15 #Duration in minutes
 NODES = "saclay,a8,106+107+102"
 
 FIRMWARE_PATH = configParser.get('exp-config', 'firware_path')
+BROKER = configParser.get('exp-config', 'broker')
+
+def add_parser_args(parser):
+	parser.add_argument('--action', 
+        dest       = 'action',
+        choices    = ['check', 'reserve', 'terminate', 'otbox', 'otbox-flash', 'ov-start', 'ov-monitor'],
+        required   = True,
+        action     = 'store'
+    )
+    parser.add_argument('--testbed', 
+        dest       = 'testbed',
+        choices    = ['iotlab', 'opentestbed'],
+        default    = 'iotlab',
+        action     = 'store'
+    )
+
+def get_args():
+	parser = argparse.ArgumentParser()
+	add_parser_args(parser)
+	args = parser.parse_args()
+
+	return {
+		'action'  : args.action,
+		'testbed' : args.testbed
+	}
 
 def main():
+	args = get_args()
+
+	action = args['action']
+	testbed = args['testbed']
+
 	print 'Script started'
-	if sys.argv[1] == '-check':
+	
+	if action == 'check':
 		print 'Checking experiment'
 		Reservation(USERNAME, HOSTNAME).check_experiment()
-	if sys.argv[1] == '-reserve':
+	if action == 'reserve':
 		print 'Reserving nodes'
 		Reservation(USERNAME, HOSTNAME).reserve_experiment(EXP_DURATION, NODES)
-	if sys.argv[1] == '-terminate':
+	if action == 'terminate':
 		print 'Terminating experiment'
 		Reservation(USERNAME, HOSTNAME).terminate_experiment()
-	elif sys.argv[1] == '-otbox':
+	elif action == 'otbox':
 		print 'Starting OTBox'
 		OTBoxStartup(USERNAME, HOSTNAME).start()
-	elif sys.argv[1] == '-otbox-flash':
+	elif action == 'otboxflash':
 		print 'Flashing OTBox'
-		OTBoxFlash(FIRMWARE_PATH).flash()
-	elif sys.argv[1] == '-ov-start':
+		OTBoxFlash(FIRMWARE_PATH, BROKER, testbed).flash()
+	elif action == 'ov-start':
 		print 'Starting OV'
 		OVStartup().start()
-	elif sys.argv[1] == '-ov-monitor':
+	elif action == 'ov-monitor':
 		print 'Starting OV log monitoring'
 		OVLogMonitor().start()
 
